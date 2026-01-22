3d6129cc1ad56715307cde381a95ae86840bb5ea
Pablo Torres <ptorres@ajleon.com>
2026-01-22 17:20:22 -0300

    ultimate refactor

--- src/models/anexasdin.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-from pydantic_xml import BaseXmlModel, element
-from .items import Anexa
-
-NSMAP_ANEXASDIN = {"": "http://www.aduana.cl/xml/esquemas/ANEXASDIN"}
-
-
-class ANEXASDIN(BaseXmlModel, nsmap=NSMAP_ANEXASDIN):
-    anexas: List[Anexa] = element(tag="ANEXA", default=[])
--- src/services/__init__.py
+++
@@ -1,4 +0,0 @@
-from .din_service import DINService
-from .xml_validator import XMLValidator
-
-__all__ = ["DINService", "XMLValidator"]
